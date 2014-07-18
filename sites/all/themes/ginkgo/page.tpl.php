9a98d19cf92552098f768190a84314e5ef6aed20
Drupal <drupal@sa-127-40.sonoma.edu>
2014-07-18 13:51:30 -0700

    Initial Repo Initialize

---
+++ sites/all/themes/ginkgo/page.tpl.php
@@ -0,0 +1,13 @@
+<?php include 'page.header.inc'; ?>
+
+<div id='content'><div class='page-region clearfix'>
+  <?php if ($action_links): ?>
+    <ul class='action-links links clearfix'><?php print render($action_links) ?></ul>
+  <?php endif; ?>
+  <?php if (!empty($page['content'])): ?>
+    <div class='page-content content-wrapper clearfix'><?php print render($page['content']) ?></div>
+  <?php endif; ?>
+</div></div>
+
+<?php include 'page.footer.inc'; ?>
+
